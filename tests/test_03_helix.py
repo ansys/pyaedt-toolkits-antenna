5627539cbffc8cae3c41106c3f9cf307e4939087
maxcapodi78 <Shark78>
2023-02-18 16:56:35 +0100

    Added Patch antennas to UI

--- tests/test_03_helix.py
+++ tests/test_03_helix.py
@@ -1,5 +1,4 @@
 from pyaedt.modeler.cad.object3d import Object3d
-from pyaedt.modeler.geometry_operators import GeometryOperators
 
 from ansys.aedt.toolkits.antennas.helix import AxialMode
 from conftest import BasisTest
@@ -22,14 +21,7 @@ class TestClass(BasisTest, object):
         assert oantenna.object_list
         for comp in oantenna.object_list.values():
             assert isinstance(comp, Object3d)
-        face_center = list(oantenna.object_list.values())[0].faces[0].center
-        assert oantenna.origin == [0, 0, 0]
-        oantenna.origin = [10, 20, 50]
-        assert oantenna.origin == [10, 20, 50]
-        face_center_new = list(oantenna.object_list.values())[0].faces[0].center
-        face_center_eval = GeometryOperators.v_sum(face_center, [10, 20, 50])
-        assert GeometryOperators.points_distance(face_center_eval, face_center_new) < 1e-6
         ohorn2 = self.aedtapp.add_from_toolkit(
-            AxialMode, draw=True, antenna_name=oantenna.antenna_name
+            AxialMode, draw=True, antenna_name=oantenna.antenna_name, outer_boundary="Radiation"
         )
         assert oantenna.antenna_name != ohorn2.antenna_name
