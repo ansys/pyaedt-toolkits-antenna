00038815080ce85f10d01d2f50cb690cf86d58c9
Samuel Lopez <samuel.lopez@ansys.com>
2023-03-29 16:17:57 +0200

    - H plane horn

--- tests/test_02_horn.py
+++ tests/test_02_horn.py
@@ -5,6 +5,7 @@ from ansys.aedt.toolkits.antennas.models.horn import ConicalHorn
 from ansys.aedt.toolkits.antennas.models.horn import CorrugatedHorn
 from ansys.aedt.toolkits.antennas.models.horn import EPlaneHorn
 from ansys.aedt.toolkits.antennas.models.horn import EllipticalHorn
+from ansys.aedt.toolkits.antennas.models.horn import HPlaneHorn
 from ansys.aedt.toolkits.antennas.models.horn import PyramidalRidged
 from conftest import BasisTest
 
@@ -176,5 +177,27 @@ class TestClass(BasisTest, object):
         assert GeometryOperators.points_distance(face_center_eval, face_center_new) < 1e-3
         ohorn2 = self.aedtapp.add_from_toolkit(
             EPlaneHorn, draw=True, antenna_name=ohorn1.antenna_name
+        )
+        assert ohorn1.antenna_name != ohorn2.antenna_name
+
+    def test_06_hplane_horn(self):
+        self.aedtapp = self.add_app(design_name="hplane_horn")
+        self.aedtapp.solution_type = "Modal"
+        ohorn1 = self.aedtapp.add_from_toolkit(
+            HPlaneHorn, draw=True, frequency=10.0, huygens_box=True
+        )
+        assert ohorn1
+        assert ohorn1.object_list
+        for comp in ohorn1.object_list.values():
+            assert isinstance(comp, Object3d)
+        face_center = list(ohorn1.object_list.values())[0].faces[0].center
+        assert ohorn1.origin == [0, 0, 0]
+        ohorn1.origin = [10, 20, 50]
+        assert ohorn1.origin == [10, 20, 50]
+        face_center_new = list(ohorn1.object_list.values())[0].faces[0].center
+        face_center_eval = GeometryOperators.v_sum(face_center, [10, 20, 50])
+        assert GeometryOperators.points_distance(face_center_eval, face_center_new) < 1e-3
+        ohorn2 = self.aedtapp.add_from_toolkit(
+            HPlaneHorn, draw=True, antenna_name=ohorn1.antenna_name
         )
         assert ohorn1.antenna_name != ohorn2.antenna_name
