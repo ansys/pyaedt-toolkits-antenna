70aff84077e6af1a2d3f671ce49ad0c78090f286
MaxJPRey <maxime.rey@ansys.com>
2023-04-19 18:25:23 +0200

    Remove importlib_metadata.

--- src/ansys/aedt/toolkits/antennas/__init__.py
+++ src/ansys/aedt/toolkits/antennas/__init__.py
@@ -4,9 +4,4 @@ pyaedt-toolkits.
 ansys.aedt.toolkits.antennas
 """
 
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
-
 __version__ = "0.1.dev0"
