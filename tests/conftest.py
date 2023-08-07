b9c1cb54f5523859940723bb927e712e2e0213f3
Samuel Lopez <samuel.lopez@ansys.com>
2023-08-07 15:17:52 +0200

    Remove launch AEDT

--- tests/conftest.py
+++ tests/conftest.py
@@ -178,7 +178,7 @@ def desktop_init():
         "use_grpc": True,
     }
     requests.put(url_call + "/set_properties", json=properties)
-    requests.post(url_call + "/launch_aedt", json=properties)
+    # requests.post(url_call + "/launch_aedt", json=properties)
     response = requests.get(url_call + "/get_status")
     while response.json() != "Backend free":
         time.sleep(1)
