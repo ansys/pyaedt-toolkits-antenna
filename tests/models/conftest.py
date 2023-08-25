c589f0de38276ebec9031a9ca70d24da0ef3be92
Samuel Lopez <samuel.lopez@ansys.com>
2023-08-25 16:53:53 +0200

    Fix UT

--- tests/models/conftest.py
+++ tests/models/conftest.py
@@ -43,7 +43,7 @@ sys.path.append(local_path)
 # from _unittest.launch_desktop_tests import run_desktop_tests
 
 # Initialize default desktop configuration
-default_version = "2023.2"
+default_version = "2023.1"
 
 config = {
     "desktop_version": default_version,
