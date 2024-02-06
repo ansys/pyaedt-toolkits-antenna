44fe28242efdf4bf167d6bdc2088a1ff6afc5a44
Kathy Pippert <kathy.pippert@ansys.com>
2024-02-06 12:40:49 -0500

    Remove prev. changes to conf.py
    Change pyproject.toml to match what Samuel sent

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -73,8 +73,6 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.coverage",
     "sphinx_copybutton",
-    "sphinx_design",
-    "sphinx_design_elements",
     "recommonmark",
     "numpydoc",
     "nbsphinx",
