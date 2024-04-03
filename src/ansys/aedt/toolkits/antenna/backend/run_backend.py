2b0e52161ade8bd26947b883c599d3e2f8a555c9
Samuelopez-ansys <samuel.lopez@ansys.com>
2024-04-03 11:07:06 +0200

    Rest API migration

--- src/ansys/aedt/toolkits/antenna/backend/run_backend.py
+++ src/ansys/aedt/toolkits/antenna/backend/run_backend.py
@@ -20,25 +20,26 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import os
+import sys
 
-from flask import request
+from ansys.aedt.toolkits.common.backend.multithreading_server import MultithreadingServer
+from ansys.aedt.toolkits.common.backend.rest_api import app
+from ansys.aedt.toolkits.common.backend.rest_api import jsonify
+from ansys.aedt.toolkits.common.backend.rest_api import logger
+from api import ToolkitBackend
 
-from ansys.aedt.toolkits.antenna.backend.api import Toolkit
-from ansys.aedt.toolkits.antenna.backend.common.multithreading_server import MultithreadingServer
-from ansys.aedt.toolkits.antenna.backend.common.rest_api_generic import app
-from ansys.aedt.toolkits.antenna.backend.common.rest_api_generic import jsonify
-from ansys.aedt.toolkits.antenna.backend.common.rest_api_generic import logger
-from ansys.aedt.toolkits.antenna.backend.common.rest_api_generic import settings
+toolkit_api = ToolkitBackend()
 
-toolkit = Toolkit()
+if len(sys.argv) == 3:
+    toolkit_api.properties.url = sys.argv[1]
+    toolkit_api.properties.port = int(sys.argv[2])
 
 
 @app.route("/create_antenna", methods=["POST"])
-def create_antenna_call():
+def create_antenna():
     logger.info("[POST] /create_antenna (create antenna in HFSS)")
 
-    properties = toolkit.get_properties()
+    properties = toolkit_api.get_properties()
 
     antenna = properties["antenna_type"]
     synth_only = properties["synth_only"]
@@ -50,26 +51,8 @@ def create_antenna_call():
         return jsonify("Antenna not created"), 500
 
 
-@app.route("/get_hfss_model", methods=["GET"])
-def get_hfss_model_call():
-    logger.info("[GET] /get_hfss_model (Get antenna model in HFSS)")
-
-    response = toolkit.export_hfss_model()
-    if response:
-        model_info = {}
-        for element in response:
-            element_path = element[0]
-            encoded_obj = toolkit._serialize_obj_base64(element_path)
-            name = os.path.splitext(os.path.basename(element_path))[0]
-            model_info[name] = [encoded_obj.decode("utf-8"), element[1], element[2]]
-
-        return jsonify(model_info), 200
-    else:
-        return jsonify("Antenna not created"), 500
-
-
 @app.route("/update_parameters", methods=["PUT"])
-def update_parameters_call():
+def update_parameters():
     logger.info("[POST] /update_parameters (Update parameters in HFSS)")
 
     body = request.json
@@ -81,7 +64,7 @@ def update_parameters_call():
     key = body["key"]
     value = body["value"]
 
-    response = toolkit.update_parameters(key, value)
+    response = toolkit_api.update_parameters(key, value)
     if response:
         return jsonify(response), 200
     else:
@@ -92,29 +75,29 @@ def update_parameters_call():
 def analyze_call():
     logger.info("[POST] /analyze (analyze AEDT project in batch)")
 
-    response = toolkit.analyze()
+    response = toolkit_api.analyze()
     if response:
         return jsonify("AEDT design launched"), 200
     else:
         return jsonify("Fail to launch design"), 500
 
 
-@app.route("/scattering_results", methods=["GET"])
-def scattering_results_call():
-    logger.info("[POST] scattering_results (Get antenna scattering results)")
+@app.route("/farfield_results", methods=["GET"])
+def farfield_results():
+    logger.info("[POST] farfield_results (Get antenna far field results)")
 
-    response = toolkit.scattering_results()
+    response = toolkit_api.farfield_results()
     if response:
         return jsonify(response), 200
     else:
         return jsonify("Fail to get results"), 500
 
 
-@app.route("/farfield_results", methods=["GET"])
-def farfield_results_call():
-    logger.info("[POST] farfield_results (Get antenna far field results)")
+@app.route("/scattering_results", methods=["GET"])
+def scattering_results():
+    logger.info("[POST] scattering_results (Get antenna scattering results)")
 
-    response = toolkit.farfield_results()
+    response = toolkit_api.scattering_results()
     if response:
         return jsonify(response), 200
     else:
@@ -122,6 +105,6 @@ def farfield_results_call():
 
 
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = toolkit_api.properties.debug
     server = MultithreadingServer()
-    server.run(host=settings["url"], port=settings["port"], app=app)
+    server.run(host=toolkit_api.properties.url, port=toolkit_api.properties.port, app=app)
