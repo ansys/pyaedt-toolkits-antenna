337dd32d36f3d482a661900f13df431ba28d5d65
Samuelopez-ansys <samuel.lopez@ansys.com>
2024-04-22 15:29:04 +0200

    CI fix I

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,5 +1,26 @@
-"""Sphinx documentation configuration file."""
+# Copyright (C) 2023 - 2024 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
+"""Sphinx documentation configuration file."""
 from datetime import datetime
 import os
 import pathlib
@@ -12,14 +33,69 @@ from ansys_sphinx_theme import get_version_match
 from ansys_sphinx_theme import latex
 from ansys_sphinx_theme import pyansys_logo_black
 from ansys_sphinx_theme import watermark
+from sphinx.util import logging
 
-sys.path.append(pathlib.Path(__file__).parent.parent.parent)
+root_path = str(pathlib.Path(__file__).parent.parent.parent)
 
-path = os.path.join(pathlib.Path(__file__).parent.parent.parent, "src")
-print(path)
-sys.path.append(path)
+try:
+    from ansys.aedt.toolkits.antenna import __version__
+except ImportError:
+    sys.path.append(root_path)
+    src_path = os.path.join(root_path, "src")
+    sys.path.append(src_path)
+    from ansys.aedt.toolkits.antenna import __version__
 
-from ansys.aedt.toolkits.antenna import __version__
+logger = logging.getLogger(__name__)
+path = pathlib.Path(os.path.join(root_path, "examples"))
+EXAMPLES_DIRECTORY = path.resolve()
+
+# Sphinx event hooks
+
+
+def check_example_error(app, pagename, templatename, context, doctree):
+    """Log an error if the execution of an example as a notebook triggered an error.
+
+    Since the documentation build might not stop if the execution of a notebook triggered
+    an error, we use a flag to log that an error is spotted in the html page context.
+    """
+    # Check if the HTML contains an error message
+    if pagename.startswith("examples") and not pagename.endswith("/index"):
+        if any(
+            map(
+                lambda msg: msg in context["body"],
+                ["UsageError", "NameError", "DeadKernelError", "NotebookError"],
+            )
+        ):
+            logger.error(f"An error was detected in file {pagename}")
+            app.builder.config.html_context["build_error"] = True
+
+
+def check_build_finished_without_error(app, exception):
+    """Check that no error is detected along the documentation build process."""
+    if app.builder.config.html_context.get("build_error", False):
+        raise Exception("Build failed due to an error in html-page-context")
+
+
+def check_pandoc_installed(app):
+    """Ensure that pandoc is installed"""
+    import pypandoc
+
+    try:
+        pandoc_path = pypandoc.get_pandoc_path()
+        pandoc_dir = os.path.dirname(pandoc_path)
+        if pandoc_dir not in os.environ["PATH"].split(os.pathsep):
+            logger.info("Pandoc directory is not in $PATH.")
+            os.environ["PATH"] += os.pathsep + pandoc_dir
+            logger.info(f"Pandoc directory '{pandoc_dir}' has been added to $PATH")
+    except OSError:
+        logger.error("Pandoc was not found, please add it to your path or install pypandoc-binary")
+
+
+def setup(app):
+    app.connect("builder-inited", check_pandoc_installed)
+    app.connect("html-page-context", check_example_error)
+    app.connect("build-finished", check_build_finished_without_error)
+
 
 print(__version__)
 # Project information
@@ -50,6 +126,7 @@ html_theme_options = {
     },
     "check_switcher": False,
     "github_url": "https://github.com/ansys/pyaedt-toolkits-antenna.git",
+    "navigation_with_keys": False,
     "show_prev_next": False,
     "show_breadcrumbs": True,
     "collapse_navigation": True,
@@ -63,8 +140,18 @@ html_theme_options = {
             "url": "https://github.com/ansys/pyaedt-toolkits-antenna/issues",
             "icon": "fa fa-comment fa-fw",
         },
+        {
+            "name": "Download documentation in PDF",
+            "url": f"https://{cname}/version/{switcher_version}/_static/assets/download/ansys-aedt-toolkits-antenna.pdf",  # noqa: E501
+            "icon": "fa fa-file-pdf fa-fw",
+        },
     ],
-    "navigation_with_keys": False,
+    "use_meilisearch": {
+        "api_key": os.getenv("MEILISEARCH_PUBLIC_API_KEY", ""),
+        "index_uids": {
+            f"pyaedt-toolkits-antenna-v{get_version_match(__version__).replace('.', '-')}": "AEDT TOOLKITS ANTENNA API",
+        },
+    },
 }
 
 # Sphinx extensions
@@ -140,40 +227,17 @@ nbsphinx_allow_errors = True
 
 # Sphinx gallery customization
 
+nbsphinx_thumbnails = {}
+
 nbsphinx_custom_formats = {
     ".py": ["jupytext.reads", {"fmt": ""}],
 }
 
 exclude_patterns = ["_build", "sphinx_boogergreen_theme_1", "Thumbs.db", ".DS_Store", "*.txt", "conf.py"]
 
-# if os.name != "posix":
-#     extensions.append("sphinx_gallery.gen_gallery")
-
-#     sphinx_gallery_conf = {
-#         # # convert rst to md for ipynb
-#         "pypandoc": True,
-#         # path to your examples scripts
-#         "examples_dirs": ["../../examples/"],
-#         # path where to save gallery generated examples
-#         "gallery_dirs": ["examples"],
-#         # Pattern to search for examples files
-#         "filename_pattern": r"\.py",
-#         # Remove the "Download all examples" button from the top level gallery
-#         "download_all_examples": False,
-#         # Sort gallery examples by file name instead of number of lines (default)
-#         "within_subsection_order": FileNameSortKey,
-#         # directory where function granular galleries are stored
-#         "backreferences_dir": None,
-#         # Modules for which function level galleries are created.  In
-#         "doc_module": "ansys-legacy",
-#         "ignore_pattern": "flycheck*",
-#         "thumbnail_size": (350, 350),
-#     }
-
-
 # -- Options for LaTeX output ------------------------------------------------
 
-# additional logos for the latex coverpage
+# additional logos for the latex coverage
 latex_additional_files = [watermark, ansys_logo_white, ansys_logo_white_cropped]
 
 # change the preamble of latex with customized title page
