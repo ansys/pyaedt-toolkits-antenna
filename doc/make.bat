1b327557b99c0da9d07502d4b9ab267541efc6de
Sébastien Morais <sebastien.morais@ansys.com>
2025-11-12 10:25:07 +0100

    CI: Remove vtk-osmesa from doc build

--- doc/make.bat
+++ doc/make.bat
@@ -10,19 +10,6 @@ if "%SPHINXBUILD%" == "" (
 set SOURCEDIR=source
 set BUILDDIR=_build
 
-REM This LOCs are used to uninstall and install specific package(s) during CI/CD
-for /f %%i in ('pip freeze ^| findstr /c:"vtk-osmesa"') do set is_vtk_osmesa_installed=%%i
-if NOT "%is_vtk_osmesa_installed%" == "vtk-osmesa" if "%ON_CI%" == "true" (
-	@ECHO ON
-	echo "Removing vtk to avoid conflicts with vtk-osmesa"
-	@ECHO OFF
-	pip uninstall --yes vtk
-	@ECHO ON
-	echo "Installing vtk-osmesa"
-	@ECHO OFF
-	pip install --index-url https://wheels.vtk.org vtk-osmesa==9.3.1
-	)
-
 if "%1" == "" goto help
 if "%1" == "clean" goto clean
 if "%1" == "pdf" goto pdf
