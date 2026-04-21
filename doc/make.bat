a9dfad67a35336f3674794792caa1a0c2ab5e7c5
Samuelopez-ansys <samuel.lopez@ansys.com>
2026-04-21 12:22:22 +0200

    Update make.bat

--- doc/make.bat
+++ doc/make.bat
@@ -11,17 +11,6 @@ set SOURCEDIR=source
 set BUILDDIR=_build
 
 REM This LOCs are used to uninstall and install specific package(s) during CI/CD
-for /f %%i in ('pip freeze ^| findstr /c:"vtk-osmesa"') do set is_vtk_osmesa_installed=%%i
-if NOT "%is_vtk_osmesa_installed%" == "vtk-osmesa" if "%ON_CI%" == "true" (
-	@ECHO ON
-	echo "Removing vtk to avoid conflicts with vtk-osmesa"
-	@ECHO OFF
-	pip uninstall --yes vtk
-	@ECHO ON
-	echo "Installing vtk-osmesa"
-	@ECHO OFF
-	pip install --index-url https://wheels.vtk.org vtk-osmesa==9.3.1
-	)
 for /f %%i in ('pip freeze ^| findstr /c:"pypandoc_binary"') do set is_pypandoc_binary_installed=%%i
 if NOT "%is_pypandoc_binary_installed%" == "pypandoc_binary" if "%ON_CI%" == "true" (
 	@ECHO ON
@@ -31,7 +20,7 @@ if NOT "%is_pypandoc_binary_installed%" == "pypandoc_binary" if "%ON_CI%" == "tr
 	@ECHO ON
 	echo "Installing pypandoc-binary"
 	@ECHO OFF
-	pip install pypandoc-binary==1.13)
+	pip install pypandoc-binary==1.15)
 REM End of CICD dedicated setup
 
 if "%1" == "" goto help
